9ddf7c1e4437ce0a9d6fe6dc06d27188dec5a717
Linh <lyntrananh@gmail.com>
2026-08-24 21:51:56 +0700

    Set up projects for e2e test suite, api test suite separately

--- tests/api.test.spec.ts
+++ tests/api.test.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, request, devices } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 
 test('TC-01: Products API returns 200 OK ', async({request})=>{
-    const response = await request.get('https://api.valentinos-magic-beans.click/products');
+    const response = await request.get('/products');
     const responseJson = await response.json();
     await expect(response.status()).toEqual(200);
     await expect(response.statusText()).toEqual('OK');
@@ -10,7 +10,7 @@ test('TC-01: Products API returns 200 OK ', async({request})=>{
 });
 
 test('TC-02: Products API returns the expected product catalog structure', async({request})=>{
-    const response = await request.get('https://api.valentinos-magic-beans.click/products');
+    const response = await request.get('/products');
     const responseJson = await response.json();
     //assert api return 200 OK
     await expect(response.status()).toBe(200);
@@ -36,7 +36,7 @@ test('TC-02: Products API returns the expected product catalog structure', async
 
 test('TC-03: Product detail API returns the correct product information', async({request})=>{
     const productId = 503;
-    const response = await request.get('https://api.valentinos-magic-beans.click/products/'+productId.toString());
+    const response = await request.get('/products/'+productId.toString());
     const responseJson = await response.json();
     await expect(response.status()).toEqual(200);
     await expect(responseJson.success).toBeTruthy();
@@ -66,7 +66,7 @@ test('TC-04: Order creation API creates an order successfully', async({request})
                                 }
                             ]
                         };
-    const response = await request.post('https://api.valentinos-magic-beans.click/orders', {
+    const response = await request.post('/orders', {
         data: JSON.stringify(orderPayload)
     });
     const responseJson = await response.json();
@@ -89,7 +89,7 @@ test('TC-05: Order lookup API returns the correct order for valid input', async(
         email:"test@abc.com"
     };
 
-    const response = await request.post('https://api.valentinos-magic-beans.click/orders/lookup', 
+    const response = await request.post('/orders/lookup', 
         {
             data: JSON.stringify(bodyPayload)
         }
@@ -107,7 +107,7 @@ test('TC-06: Order lookup API returns a not-found response for an invalid order 
         orderId:"test2B42062A",
         email:"test@abc.com"
     };
-    const response = await request.post('https://api.valentinos-magic-beans.click/orders/lookup', {
+    const response = await request.post('/orders/lookup', {
         data: JSON.stringify(bodyPayload)
     });
     const responseJson = await response.json();
@@ -118,7 +118,7 @@ test('TC-06: Order lookup API returns a not-found response for an invalid order 
 
 test('TC-07: Product detail API returns a not-found response for an invalid product ID', async({request})=>{
     const invalidProductID = 1000;
-    const response = await request.get('https://api.valentinos-magic-beans.click/products/' + invalidProductID);
+    const response = await request.get('/products/' + invalidProductID);
     const responseJson = await response.json();
 
     await expect(response.status()).toBe(404);
