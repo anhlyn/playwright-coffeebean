f0ddc13edf0f9cfa2688c5564b43f80582907076
Linh <lyntrananh@gmail.com>
2026-08-25 21:57:56 +0700

    Applied pom into TCs 04, 05, 06

--- tests/e2e.test.spec.ts
+++ tests/e2e.test.spec.ts
@@ -1,6 +1,8 @@
 import {test, expect} from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import { HomePage } from './pages/homepage';
+import { ProductPage } from './pages/productpage';
+import { CartPage } from './pages/cartpage';
 
 test.describe('A. Navigation and Homepage', ()=>{
     test('TC-01: Home page loads successfully', async({page})=>{
@@ -53,98 +55,88 @@ test.describe('B. Product Catalog and Details',()=>{
     });
 
     test('TC-04: User can open a product detail page', async({page})=>{
-
+        const homepage = new HomePage(page);
+        const productpage = new ProductPage(page);
         await test.step('Step1: Navigate to /products', async()=>{
-            await page.goto('/');
-            await page.locator('nav').getByRole('link', {name: 'Shop'}).click();
-            await expect(page).toHaveURL(/products/);
+            await homepage.gotoHomePage();
+            await productpage.goToProductPage();
+            await productpage.verifyOnProductPage();
         });
 
-        const firstProduct = page.locator('[data-test-id^="product-card-"]').first();
-        const firstProductName = await firstProduct.getByRole('heading').textContent();
-        const firstProductPrice = await firstProduct.locator('span.text-2xl').textContent();
-
+        const firstProduct = await productpage.getFirstProduct();
         await test.step('Step2: click button "View Details"', async()=>{
-            await firstProduct.getByRole('button', {name: 'View Details'}).click();
-            await expect(page.getByRole('heading', {name: 'Product Details'})).toBeVisible();
+            await productpage.clickViewDetail(firstProduct.locator);
         });
-
         await test.step('Step3: Verify test result', async()=>{
-            await expect(page.getByRole('heading', {name: firstProductName ?? ''})).toBeVisible();
-            await expect(page.locator('p.text-3xl').filter({hasText: firstProductPrice ?? ''})).toBeVisible();
+            await productpage.verifyProductDetailsHeading(firstProduct.name??'');
+            await productpage.verifyPrice(firstProduct.price??'');
         });
     });
 
     test('TC-05: User can add a product to the cart', async({page})=>{
+        const homepage = new HomePage(page);
+        const productpage = new ProductPage(page);
+        const cartpage = new CartPage(page);
+
         await test.step('Step: Go to /products', async()=>{
-            await page.goto('/');
-            await page.locator('nav').getByRole('link', {name: 'Shop'}).click();
-            await expect(page).toHaveURL(/products/);
+            await homepage.gotoHomePage();
+            await productpage.goToProductPage();
+            await productpage.verifyOnProductPage();
         });
-
-        const productName = 'Ethiopian Highlands';
-        const productDiv = await page.locator('div[data-test-id^="product-card-"]').filter({has: page.getByRole('heading', {name: productName})});
-        const productPrice = await productDiv.locator('span').textContent();
-        const btnAddToCart = productDiv.getByRole('button', {name: 'Add to Cart'});
-
+        const prodName = 'Ethiopian Highlands';
         await test.step('Step2: click "Add to Cart"', async()=>{
-            await btnAddToCart.click();
+            await productpage.addToCart(prodName);
         });
-
-        //Assertion
-        //clicking the Cart icon at the top corner of the page
         await test.step('Step3: click icon Cart at the top-right corner of the page', async()=>{
-            await page.locator('a[data-test-id="header-cart-button"]>button').click();
-            await expect(page).toHaveURL(/cart/); 
+            await cartpage.clickHeaderCartIconOnTheTop();
+            await cartpage.verifyOnCartPage();
         });
-
         await test.step('Step4: Verify the test result', async()=>{
-            const cartItemDiv = page.locator('[data-test-id="cart-item"]').first();
-            await expect(cartItemDiv.getByRole('heading', {name: productName})).toBeVisible();
-            await expect(cartItemDiv.locator(".text-right>p")).toContainText(productPrice ?? '');
+            await cartpage.verifyProductNameIsInCart(prodName);
         });
     });
 
     test('TC-06: Cart page displays selected items (more than 1 product) correctly', async({page})=>{
         let products = [
             {
-                name: 'Italian Dark Roast',
+                name: 'Colombian Supreme',
                 price: ''
             },
             {
-                name: 'Brazilian Santos',
+                name: 'Guatemalan Volcano',
                 price: ''
             }
         ];
+
+        const homepage = new HomePage(page);
+        const productpage = new ProductPage(page);
+        const cartpage = new CartPage(page);
         await test.step('Step1: go to /products', async()=>{
-            await page.goto('/');
-            await page.locator('nav').getByRole('link', {name: 'Shop'}).click();
-            await expect(page).toHaveURL(/products/);
+            await homepage.gotoHomePage();
+            await productpage.goToProductPage();
+            await productpage.verifyOnProductPage();
         });
 
         await test.step('Step2: Add selected products to the cart', async()=>{
             for(let product of products){
-                const productDiv = await page.locator('div[data-test-id^="product-card-"]').filter({has: page.getByRole('heading', {name: product.name})});
-                product.price = await productDiv.locator('span').textContent() ?? '';
-                const btnAddToCart = productDiv.getByRole('button', {name: 'Add to Cart'});
-                await btnAddToCart.click();
+                const tempProd = await productpage.addToCart(product.name);
+                product.price = tempProd.productPrice??'';
             } 
         });
 
         await test.step('Step3: navigate to the /cart', async()=>{
-            await page.locator('[data-test-id="header-cart-button"]').click();
-            await expect(page).toHaveURL(/cart/);
+            await cartpage.clickHeaderCartIconOnTheTop();
+            await cartpage.verifyOnCartPage();
         });
 
         await test.step('Step4: Verify test result', async()=>{
-            const locatorCartItems = await page.locator('[data-test-id="cart-item"]').all();
-            let i = 0;
             for(let product of products){
-                await expect(locatorCartItems[i].getByRole('heading', {name: product.name})).toBeVisible();
-                expect(await locatorCartItems[i].locator('div.text-right>p').textContent()).toBe(product.price);
-                i++;
+                await cartpage.verifyProductNameIsInCart(product.name);
+                await cartpage.verifyProductPriceIsInCart(product.price);
             }
         });
+
+        await page.pause();
     });
 });
 
